a22688b5ab7932c5476e88b048e1637b0341a468
Antonios Papadakis <1001545@isd535.org>
2020-04-18 22:14:20 -0500

    clean up the code
    shorten the code and hopefully slightly increase performance. who knows. not me.

--- script.js
+++ script.js
@@ -22,31 +22,26 @@ for(var i = 0; i < questions.length; i++) { //cycles through asking all the ques
     while(response !== questions[i].answer) { // Primitive Artificial Inteligence for computer responses based on score :)
         if(score >= 8) {
             var aiResponse = "Good Job! You did very well in this trivia game. Go brag to your friends!";
-            alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
             break;
         }
         
         else if(score <= 7 && score >= 5) {
             var aiResponse = "You did pretty good. You just need some minor touchups in studying to get a better score.";
-            alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
             break;
         }
 
         else if(score <= 5 && score >= 3) {
             var aiResponse = "You didn't do so good. I wouldn't suggest bragging to your friends.";
-            alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
             break;
         }
 
         else if(score <= 3 && score >= 1){
             var aiResponse = "You did REALLY bad. Like...BAD. Don't show your friends.";
-            alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
             break;
         }
 
         else if(score <= 1) {
             var aiResponse = "Just stop. Ok? Get out. Have you been living under a rock? You shouldn't even be here right now. Definitely DON'T show your friends...unless it's too late.";
-            alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
             break;
         }
 
@@ -56,3 +51,5 @@ for(var i = 0; i < questions.length; i++) { //cycles through asking all the ques
         }
     }
 }
+
+alert("You got " + score +"/" + questions.length + " questions right. " + aiResponse);
