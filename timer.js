7e1a6da6ce1192d5bbfb77cd4fa5b07689e7e78a
Antonios Papadakis <1001545@isd535.org>
2020-04-19 12:36:00 -0500

    improve the countdown timer

--- timer.js
+++ timer.js
@@ -1,11 +1,22 @@
-var count = 30;
-var interval = setInterval(function(){
-  document.getElementById('count').innerHTML=count;
-  count--;
-  if (count === 0){
-    clearInterval(interval);
-    document.getElementById('count').innerHTML='Done';
-    // or...
-    alert("You're out of time!");
-  }
-}, 1000);
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var halfMinute = 60 * 5,
+        display = document.querySelector('#time');
+    startTimer(halfMinute, display);
+};
