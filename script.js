bc4f9880f702d901ab52ce97fcf825d243fb0c0f
Antonios Papadakis <1001545@isd535.org>
2020-04-18 21:47:10 -0500

    Increase gameplay immersive ness
    add Primitive AI Technologies™ to increase immersive ness.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var questions = [
     }, //repeat for the rest of the questions. make sure you put the questions within the array.
 ];
 
+var aiResponse = "";
 var score = 0;
 
 for(var i = 0; i < questions.length; i++) { //cycles through asking all the questions
@@ -12,10 +13,32 @@ for(var i = 0; i < questions.length; i++) { //cycles through asking all the ques
 
     if(response == questions[i].answer){ //checks for right answer
         score++;
-        alert("Correct!"); //correct answer!
+        alert("Correct!"); //CORRECT ANSWER!
     }
     else {
-        alert("WRONG!") //WRONG ANSWER!
+        alert("WRONG!"); //WRONG ANSWER!
+    }
+
+    while(response !== questions[i].answer) { // Primitive Artificial Inteligence for computer responses based on score :)
+        if(score > 8) {
+            var aiResponse = "Good Job! You did very well in this trivia game. Go brag to your friends!";
+        }
+        
+        if(score < 7) {
+            var aiResponse = "You did pretty good. You just need some minor touchups in studying to get a better score.";
+        }
+
+        if(score < 5) {
+            var aiResponse = "You didn't do so good. I wouldn't suggest bragging to your friends.";
+        }
+
+        if(score < 3){
+            var aiResponse = "You did REALLY bad. Like...BAD. Don't show your friends.";
+        }
+
+        if(score < 1) {
+            var aiResponse = "Just stop. Ok? Get out. Have you been living under a rock? You shouldn't even be here right now. Definitely DON'T show your friends...unless it's too late.";
+        }
     }
 }
-alert("You got" + score +"/" + questions.length + " questions right.")
+alert("You got" + score +"/" + questions.length + " questions right. " + aiResponse)
