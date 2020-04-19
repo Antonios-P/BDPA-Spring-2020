a83390caae59bbc82122a143eafa9cf600d25f65
Antonios Papadakis <1001545@isd535.org>
2020-04-19 13:22:29 -0500

    improve countdown timer and remove debugging comment

--- timer.js
+++ timer.js
@@ -1,22 +1,24 @@
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
+    var timer = duration,
+      minutes, seconds;
+    setInterval(function() {
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        display.textContent = minutes + ":" + seconds;
+      display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
+      if (--timer < 0) {
+        timer = duration;
+        alert("Time is up!")
+      }
     }, 1000);
-}
+  }
 
-window.onload = function () {
-    var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-    startTimer(fiveMinutes, display);
-};
+  window.onload = function() {
+    var halfMinute = 30,
+      display = document.querySelector('#time');
+    startTimer(halfMinute, display);
+  };
