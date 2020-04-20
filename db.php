5963bebc59309bdf181b1eb7926eafe359e8e2a0
Antonios Papadakis <1001545@isd535.org>
2020-04-19 21:25:50 -0500

    update db.php for current database

--- db.php
+++ db.php
@@ -2,5 +2,5 @@
 //connected.php
 $username = 'p1001545_labs';
 $password = '5O]JA%a7m4@J1Kyt';
-$db = new PDO('mysql:host=localhost;dbname=p1001545_bank_loan2020', $username, $password);
-$dbStmt = new PDOStatement('mysql:host=localhost;dbname=p1001545_bank_loan2020', $username, $password);
+$db = new PDO('mysql:host=localhost;dbname=p1001545_spring2020', $username, $password);
+$dbStmt = new PDOStatement('mysql:host=localhost;dbname=p1001545_spring2020', $username, $password);
