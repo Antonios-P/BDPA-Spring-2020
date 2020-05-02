ca76aa981480b315748230439ae69e0f3f8b3196
Jacob <jakeburner.jt@gmail.com>
2020-05-02 16:23:26 -0500

    file that has ideas will be used for test later

---
+++ test.php
@@ -0,0 +1,33 @@
+<?php
+
+    //game id auto incriment 
+
+    //switch statement 
+
+    //case for different parts (create player ect)
+
+    //or maybe split it up into different pages
+
+    //create something that will generate question/answer ids with a game 
+
+    //link those ids to the bank of questions/answers
+
+    //Room keys for game? (not too sure how erik did it)
+
+    //a way to chedk the players answer if it is correct or not
+
+    //point system for when that happens
+
+    //a way to have players interacting with eachother 
+
+    //maybe a timer of 30 seconds then it shows the players answers and total points (maybe colors to show if they got it right or not?)
+
+    //possibly a way to detect if all players have answered and if so it skips the 30 second timer
+
+    //some kind of lobby for players to join in
+
+    //MAYBE a way to kick players
+
+    
+
+?>
