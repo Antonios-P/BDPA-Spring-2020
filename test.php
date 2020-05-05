221a1e76a40e148668094973a5adf8f5ea13e971
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-05 15:21:09 -0500

    add test.php just like cPanel and cleaned it up

--- test.php
+++ test.php
@@ -1,13 +1,68 @@
+<?php
 
+    //game id auto incriment 
 
-<body>
-    <link rel="stylesheet" type="text/css" href="style.css">
-<div>test timer runs out in <span id="time">00:30</span> minutes!</div>
-    <form method="GET">
-        Response: <input type="text" name="response">
-        <button type="submit" class="button-green" value="submit">Submit Answer</button>
-    </form>
-                    
-</body>
-<script src="game.js"></script>
+    //switch statement 
+
+    //case for different parts (create player ect)
+
+    //or maybe split it up into different pages
+
+    //or split into pages and use include to put them into a switch statement 
+
+    //create something that will generate question/answer ids with a game 
+
+    //link those ids to the bank of questions/answers
+
+    //Room keys for game? (not too sure how erik did it)
+
+    //a way to chedk the players answer if it is correct or not
+
+    //point system for when that happens
+
+    //a way to have players interacting with eachother 
+
+    //maybe a timer of 30 seconds then it shows the players answers and total points (maybe colors to show if they got it right or not?)
+
+    //possibly a way to detect if all players have answered and if so it skips the 30 second timer
+
+    //some kind of lobby for players to join in
+
+    //MAYBE a way to kick players
+
+    /* 
+
+    create a game
+
+        sql AI for game id
+
+        game code random generated 
+
+        current time for time started
+
+    game answers
+
+        connect game id with the one above
+
+        im not too sure on how to generate the question id/answer thing
+
+        connect question id question bank/same with answerid
+
+    */
+
+    require 'db.php';
+    
+    echo 'hello';
+    
+    $gamecode = uniqid();
+
+   // $insert = "INSERT INTO `Games` (`GameCode`) VALUES ('$gamecode')";
+
+   // $statement = $db-> prepare($insert);
+                
+   // $statement -> execute();
+    
+    echo 'Statement executed sucessfully';
+
+?>
 
