2deddb6c6af71ca2096769a48a19b3ec4d2af40e
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-06 13:14:14 -0500

    fixed errors and added table
    The table doesn't select from the database still for some reason. No errors in error log.

--- all-games.php
+++ all-games.php
@@ -3,20 +3,34 @@
 require 'db.php';
 
 $result = "SELECT * FROM Games";
-$result = $db->prepare($query);
- 
-if ($result = $db->execute()) {
+$statement = $db->prepare($query);
+$statement->execute();
+
+echo '<table align="center" width=auto border="0" cellspacing="2" cellpadding="2"> 
+      <tr> 
+          <td> <font face="Arial">Game ID</font> </td> 
+          <td> <font face="Arial">Game Code</font> </td> 
+          <td> <font face="Arial">Time Started</font> </td> 
+          <td> <font face="Arial">Description</font> </td> 
+      </tr>';
  
     /* fetch associative array */
-    while ($row = $result->FETCH_ASSOC()) {
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
         $field1name = $row["GameId"];
         $field2name = $row["GameCode"];
         $field3name = $row["TimeStarted"];
         $field4name = $row["GameDesc"];
+
+        echo '<tr> 
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+                  <td>'.$field3name.'</td> 
+                  <td>'.$field4name.'</td> 
+              </tr>';
+
     }
  
     /* free result set */
     $result->closeCursor();
-}
 
 ?>
