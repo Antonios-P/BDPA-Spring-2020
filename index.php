beed2565207e2bd01aede0433eddfeb91c26d229
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-07 12:43:25 -0500

    update index, improve styling
    added background image

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <body>
-    <div class="parallax">
-        Welcome to Kapoop! Please use the navigation  bar to travel to different parts of our website. Enjoy your stay!
+    <div>
+        Welcome to Kapoop! Please use the navigation  bar to travel to different parts of our website or use the buttons below. Enjoy your stay!
+        
+        <br>
+        <br>
+        <br>
+
+        <a href="all-games.php">Browse All Games</a>
+        <a href="create-game.php">Create a Game</a>
     </div>
 </body>
