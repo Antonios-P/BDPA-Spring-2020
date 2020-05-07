5228ea252bb5da9abcedd10331d23bdb824460a4
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-07 11:48:00 -0500

    add welcome message to the index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-<?php 
-
-
-
-?>
+<body>
+    <div class="parallax">
+        Welcome to Kapoop! Please use the navigation  bar to travel to different parts of our website. Enjoy your stay!
+    </div>
+</body>
