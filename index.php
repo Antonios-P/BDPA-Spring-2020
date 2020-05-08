4b6e3d5cd07ceaebd6e847483b2650046130a6af
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-08 10:09:49 -0500

    minor styling changes to the home page

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php require 'depends/header.php'; ?>
 
 <body>
-    <div>
+    <div align="center" style="color: white;">
         Welcome to Kapoop! Please use the navigation  bar to travel to different parts of our website or use the buttons below. Enjoy your stay!
         
         <br>
         <br>
         <br>
 
-        <a href="all-games.php">Browse All Games</a>
-        <a href="depends/create-game.php">Create a Game</a>
+        <a class="button-blue" align="center" href="all-games.php">Browse All Games</a>
+        <a class="button-green" align="center" href="depends/create-game.php">Create a Game</a>
     </div>
 </body>
 
