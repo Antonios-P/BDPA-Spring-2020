0938ff25d7c19f26f9a04bcab4e616e883130fc3
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-09 13:47:13 -0500

    update the form action

--- depends/create-game-form.php
+++ depends/create-game-form.php
@@ -3,7 +3,7 @@
 <?php 
 echo '
 <div class="divForm">
-    <form align="center" action="" method="POST">
+    <form align="center" action="create-game.php" method="POST">
         What is your name? <br><input type="text" name="dispname"><br>
         How many questions would you like the game to have? <br><input type="text" name="numberofquestions"><br>
         <input class="" type="submit" value="Create Game">
