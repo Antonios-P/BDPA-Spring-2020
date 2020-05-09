9f9a0a30a3d60dffe509be16efed6109e7d994aa
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-09 13:42:58 -0500

    add game creation form

---
+++ depends/create-game-form.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" type="text/css" href="https://bdpastudents.com/~team2/spring2020/BDPA-Spring-2020/style.css">
+
+<?php 
+echo '
+<div class="divForm">
+    <form align="center" action="" method="POST">
+        What is your name? <br><input type="text" name="dispname"><br>
+        How many questions would you like the game to have? <br><input type="text" name="numberofquestions"><br>
+        <input class="" type="submit" value="Create Game">
+    </form> 
+</div>';
+
+?>
