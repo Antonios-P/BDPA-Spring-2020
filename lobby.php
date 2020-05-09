af33b381fc93adff502dc7729244c826319013b7
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-09 18:53:05 -0500

    add php code needed to query number of players

--- lobby.php
+++ lobby.php
@@ -1,5 +1,15 @@
 <?php 
+require 'depends/db.php';
 
+//query from the database
+$players = $db -> prepare("SELECT * FROM GamePLayers WHERE GameID = ?");
+$placeholders = [
+    $_SESSION['GameID']
+]; 
+$players->execute($placeholders);
 
+$numberofplayers = $players->rowCount();
+
+echo '<script src="depends/getnumberofplayers.js"></script>'
 
 ?>
