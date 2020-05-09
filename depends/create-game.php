2184e1bbaea4a6306829a370597c533202d45f5d
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-09 12:40:15 -0500

    update to use db.php

--- depends/create-game.php
+++ depends/create-game.php
@@ -1,10 +1,6 @@
 <?php
 
-    $username = 'team2_2';
-    
-    $password = 'TPH5zOSt]&oY';
-    
-    $db = new PDO('mysql:host=localhost;dbname=team2_trivia', $username, $password);
+    require 'depends/db.php';
     
     $gamecode = uniqid();
 
