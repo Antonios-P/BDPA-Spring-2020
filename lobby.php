9dcc992f9821447060e0c311ba3b0ffe0fe7558b
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-10 16:27:04 -0500

    remove commented code
    found in getplayers.php

--- lobby.php
+++ lobby.php
@@ -1,16 +1,4 @@
 <?php 
-/*require 'depends/db.php';
-
-//query from the database
-$players = $db -> prepare("SELECT * FROM GamePLayers WHERE GameID = ?");
-$placeholders = [
-    $_SESSION['GameID']
-]; 
-$players->execute($placeholders);
-
-$numberofplayers = $players->rowCount();
-
-echo '<script src="depends/getnumberofplayers.js"></script>'*/
 
     session_start();
     
