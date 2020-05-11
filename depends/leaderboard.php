2dfbac4d2e9beb8b443016bb1ce1166276a820ee
Antonios Papadakis <bdpaboi123@gmail.com>
2020-05-11 14:16:39 -0500

    add styling to leaderboard

--- depends/leaderboard.php
+++ depends/leaderboard.php
@@ -1,3 +1,5 @@
+<link rel="stylesheet" type="text/css" href="style.css">
+
 <?php 
 require 'db.php';
 
@@ -6,22 +8,32 @@ $i = 1;
 $score = $db -> prepare("SELECT PlayerScore FROM GamePlayers ORDER BY PlayerScore DESC LIMIT 5");
 $score -> execute();
 
-echo '<table style="color: white;" class="games" align="center" width=auto border="0" cellspacing="2" cellpadding="2"> 
+echo '<div id="container">';
+
+echo '<table style="color: white;" class="games" align="center" width=auto border="0" cellspacing="2" cellpadding="2">'; 
+echo '<div class="row">';
+    echo '
       <tr> 
           <th> <font face="Arial">Place</font> </th>
           <th> <font face="Arial">Score</font> </th>
       </tr>';
+echo '</div>';
 
 while ($row = $score->fetch(PDO::FETCH_ASSOC)) {
     $field1name = $row["PlayerScore"];
 
+    echo '<div class="row">';
     echo '<tr> 
             <td>'.$i.'</td>
             <td>'.$field1name.'</td>
           </tr>';
 
+    echo '</div>';
+
     $i++;
 }
+
+echo '</div>';
 
 
 ?>
